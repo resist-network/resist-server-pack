b0abdded1443cb791eac9439c1df9256ff657fdd
mindamp <mindamp@gmail.com>
2019-01-22 03:25:17 +0000

    Automation, bleh!

--- start.sh
+++ start.sh
@@ -11,6 +11,10 @@ MYSQL_HOST=$(cat ~/.mysql_host)
 IRC_PASSWORD=$(cat ~/.irc_password)
 MCR_PASS=$(cat ~/.mcr_pass)
 MONGO=$(cat ~/.mongo)
+JAR="forge-1.12.2-14.23.5.2807-universal.jar"
+RAM_MIN="512M"
+RAM_MAX="12G"
+CPU_CORES="5"
 #CURRENT_VERSION="$(curl -s 'https://raw.githubusercontent.com/worldautomation/WA-Launcher-Pack/master/app/assets/distribution.json' | awk '/version/{i++}i==2{print; exit}' | awk -F "\"*:\"*" '{print $2}' | cut -c 3- | cut -c -7)"
 if ! screen -list | grep -q "resist"; then
 	echo "Server is starting!"
@@ -99,7 +103,7 @@ if ! screen -list | grep -q "resist"; then
 	rm server.properties
 	sed s/MCR_PASS/$MCR_PASS/g server.properties.template > server.properties
 	#sudo screen -dmS resist sudo java -Xmx8G -Xms6G -jar forge-1.12.2-14.23.5.2768-universal.jar nogui
-	sudo screen -dmS resist sudo java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=3 -XX:+AggressiveOpts -Xms1G -Xmx8G -jar forge-1.12.2-14.23.5.2807-universal.jar nogui
+	sudo screen -dmS resist sudo java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=$CPU_CORES -XX:+AggressiveOpts -Xms$RAM_MIN -Xmx$RAM_MAX -jar $JAR nogui
 else
 	echo "Server already started!";
 fi
