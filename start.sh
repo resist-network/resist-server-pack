fef8be2aa047f44c8a5759119db660a7638b6e83
mindamp <mindamp@gmail.com>
2019-02-09 03:56:16 +0000

    Automation, bleh!

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 rm -rf logs/*.gz
+DISCORD=$(cat ~/.discord_token)
 MCR_PASS=$(cat ~/.mcr_pass)
 JAR="forge-1.12.2-14.23.5.2807-universal.jar"
 RAM_MIN="1G"
 RAM_MAX="4G"
 CPU_CORES="2"
+#Chikachi/discordintegration.json.template
 #CURRENT_VERSION="$(curl -s 'https://raw.githubusercontent.com/worldautomation/WA-Launcher-Pack/master/app/assets/distribution.json' | awk '/version/{i++}i==2{print; exit}' | awk -F "\"*:\"*" '{print $2}' | cut -c 3- | cut -c -7)"
 if ! screen -list | grep -q "resist"; then
 	echo "Server is starting!"
